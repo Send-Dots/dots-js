9ccb8b347cb78a3ed8fc8aa481aa95b84f501d1d
Jack Li <jack@dots.dev>
2023-04-23 23:49:55 -0700

    typing fix

--- types/api/payment-methods.d.ts
+++ types/api/payment-methods.d.ts
@@ -2,7 +2,7 @@ import { DotsElement } from '../dots-js';
 
 export type PaymentMethod = {
   element: DotsElement;
-  id: string;
+  id?: string;
   billing_details: {
     name?: string;
     email?: string;
